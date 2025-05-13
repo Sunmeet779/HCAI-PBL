8728c8aca44a1724773079f965d9d4d16471a3fb
Sanika Acharya <sanacharya2001@gmail.com>
2025-05-13 23:12:07 +0200

    Added new projects

--- project1/views.py
+++ project1/views.py
@@ -1,17 +1,5 @@
-import pandas as pd
 from django.shortcuts import render
-from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, JsonResponse
 
 def index(request):
-    return render(request, 'project1/index.html')
-
-def upload_file(request):
-    if request.method == 'POST' and request.FILES['dataset']:
-        dataset = request.FILES['dataset']
-        fs = FileSystemStorage()
-        filename = fs.save(dataset.name, dataset)
-        df = pd.read_csv(fs.path(filename))
-
-        request.session['data_path'] = fs.path(filename)  # Save path for reuse
-        return render(request, 'project1/display.html', {'df': df.to_html()})
-    return render(request, 'project1/upload.html')
+    return HttpResponse("Welcome to Project 1!")
