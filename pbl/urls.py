c3c9e2ad1c3e4dfbde7466d3624a020b3211d624
Sunmeet779 <75309251+Sunmeet779@users.noreply.github.com>
2025-05-13 23:04:38 +0200

    creating skeleton for all projects

--- pbl/urls.py
+++ pbl/urls.py
@@ -21,7 +21,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", include("home.urls")),
     path("home/", include("home.urls")),
     path("admin/", admin.site.urls),
     path("demos/", include("demos.urls")),
+    path("project1/", include("project1.urls")),
+    path("project2/", include("project2.urls")),
+    path("project3/", include("project3.urls")),
+    path("project4/", include("project4.urls")),
+    path("project5/", include("project5.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
