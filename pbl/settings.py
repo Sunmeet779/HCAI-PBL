c0e9811aec33bf76159b5dbcbf21ac9d15d86415
Sanika Acharya <sanacharya2001@gmail.com>
2025-08-21 18:57:02 +0200

    allowed hosts

--- pbl/settings.py
+++ pbl/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ulscw9yso6q+hyj(2s-3ubvd9=krd-4p!axal6962&_meb9w&o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
